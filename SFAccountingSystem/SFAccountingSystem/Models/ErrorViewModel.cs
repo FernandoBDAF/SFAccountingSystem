d868064274747ecb17ab34b43ad9e5e51c40bf26
FernandoBDAF <110940005+FernandoBarrosoDAF@users.noreply.github.com>
2023-03-03 17:53:27 -0300

    Initial Code

---
+++ SFAccountingSystem/SFAccountingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SFAccountingSystem.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
