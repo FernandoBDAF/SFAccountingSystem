8288dc06d6d509cdefd6096ba5e1a1bbf5e31b86
FernandoBDAF <110940005+FernandoBarrosoDAF@users.noreply.github.com>
2023-03-08 13:57:23 -0300

    Created DataContext.cs

---
+++ SFAccountingSystem/SFAccountingSystem/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SFAccountingSystem
+{
+	public class DataContext : DbContext
+	{
+		DataContext(DbContextOptions<DataContext> options) : base(options) 
+		{
+			// Add variables for each table public DbSet<class> Class { get; set; }
+		}
+	}
+}
