e02c6d4e48db6fd64856457be6744bef91f0266c
FernandoBDAF <110940005+FernandoBarrosoDAF@users.noreply.github.com>
2023-03-09 18:07:56 -0300

    new controller: RecordOFXSubGroup

---
+++ SFAccountingSystem/SFAccountingSystem/Controllers/RecordOFXSubGroup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFAccountingSystem.Controllers
+{
+    public class RecordOFXSubGroup : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
