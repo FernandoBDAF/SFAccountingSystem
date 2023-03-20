ae33250b6a670f1c908d39a8fb069843ee591849
lucasviniciusbatistacosta <lucasviniciusbatistacosta@gmail.com>
2023-03-20 20:31:53 -0300

    update title.

--- SFAccountingSystem/SFAccountingSystem/Controllers/RecordOFXSubGroupController.cs
+++ SFAccountingSystem/SFAccountingSystem/Controllers/RecordOFXSubGroupController.cs
@@ -1,20 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SFAccountingSystem.Enums;
+using SFAccountingSystem.Models;
 
 namespace SFAccountingSystem.Controllers
 {
     public class RecordOFXSubGroupController : Controller
     {
+        private ILogger<RecordOFXSubGroupController> _logger;
         private readonly DataContext _dataContext;
 
-        public RecordOFXSubGroupController(DataContext dataContext)
+        public RecordOFXSubGroupController(DataContext dataContext, ILogger<RecordOFXSubGroupController> logger)
         {
             _dataContext = dataContext;
+            _logger = logger;
         }
 
         public async Task<IActionResult> Index(RecordOFXGroup? groupId = null, int? parentId = null)
         {
+            ViewData["Title"] = "Groups";
+
             if (groupId == null)
             {
                 Dictionary<string, int> groups = new();
@@ -30,11 +36,76 @@ namespace SFAccountingSystem.Controllers
             else if (groupId.HasValue)
             {
                 ViewBag.Group = groupId;
+                ViewBag.ParentId = parentId;
+
+                ViewData["Title"] += $" > {groupId.ToName<RecordOFXGroup>()}";
+
                 return View("IndexSubGroup", await _dataContext.RecordOFXSubGroups
                                                                .Include(x => x.RecordsOFXes)
                                                                .Include(x => x.ChildRecordsOFXes)
                                                                .Where(x => x.Group == groupId && x.ParentId == parentId)
                                                                .ToListAsync());
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<IActionResult> Create(RecordOFXGroup groupId, int? parentId = null)
+        {
+            await Task.Yield();
+
+            ViewBag.Groups = new SelectList(Enum.GetValues(typeof(RecordOFXGroup))
+                                                .Cast<RecordOFXGroup>()
+                                                .Select(x => new
+                                                {
+                                                    DisplayName = x.ToName<RecordOFXGroup>(),
+                                                    Id = (int)x
+                                                }), "Id", "DisplayName");
+
+            return View(new RecordOFXSubGroup
+            {
+                Group = groupId,
+                ParentId = parentId
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RecordOFXSubGroup model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    ViewBag.Groups = new SelectList(Enum.GetValues(typeof(RecordOFXGroup))
+                                                        .Cast<RecordOFXGroup>()
+                                                        .Select(x => new
+                                                        {
+                                                            DisplayName = x.ToName<RecordOFXGroup>(),
+                                                            Id = (int)x
+                                                        }), "Id", "DisplayName");
+
+                    return View(model);
+                }
+                else if (ModelState.IsValid)
+                {
+                    await _dataContext.RecordOFXSubGroups.AddAsync(model);
+                    await _dataContext.SaveChangesAsync();
+
+                    return RedirectToAction("Index", new { parentId = model.ParentId, groupId = model.Group });
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Groups = new SelectList(Enum.GetValues(typeof(RecordOFXGroup))
+                                                    .Cast<RecordOFXGroup>()
+                                                    .Select(x => new
+                                                    {
+                                                        DisplayName = x.ToName<RecordOFXGroup>(),
+                                                        Id = (int)x
+                                                    }), "Id", "DisplayName");
+                ModelState.AddModelError("", ex.Message);
+
+                return View(model);
             }
 
             throw new NotImplementedException();
