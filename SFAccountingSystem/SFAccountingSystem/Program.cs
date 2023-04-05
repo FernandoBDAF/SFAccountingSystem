6abc444a85d70ea5900f1ca6375c57030ae7d234
lucasviniciusbatistacosta <lucasviniciusbatistacosta@gmail.com>
2023-04-05 14:39:13 -0300

    Add ofx service.

--- SFAccountingSystem/SFAccountingSystem/Program.cs
+++ SFAccountingSystem/SFAccountingSystem/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SFAccountingSystem;
+using SFAccountingSystem.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,17 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<DataContext>(option =>
-	option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-	o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+    o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
+
+builder.Services.AddScoped<OfxService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -28,7 +31,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
