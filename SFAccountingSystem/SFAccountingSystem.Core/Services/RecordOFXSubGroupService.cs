d50a4e2a2fc11b3220bb9f67d7bf98b2c179c0cc
lucasviniciusbatistacosta <lucasviniciusbatistacosta@gmail.com>
2023-04-10 19:21:16 -0300

    update record ofx subgroup.

--- SFAccountingSystem/SFAccountingSystem.Core/Services/RecordOFXSubGroupService.cs
+++ SFAccountingSystem/SFAccountingSystem.Core/Services/RecordOFXSubGroupService.cs
@@ -12,10 +12,49 @@ namespace SFAccountingSystem.Core.Services
 
         public async Task<List<RecordOFXSubGroup>> List(RecordOFXGroup groupId)
         {
-            return await context.RecordOFXSubGroups
-                                .Where(x => x.Group == groupId)
-                                .OrderBy(x => x.ParentId)
-                                .ToListAsync();
+            var list = new List<RecordOFXSubGroup>();
+
+
+            var query = await context.RecordOFXSubGroups
+                                        .Where(x => x.Group == groupId && !x.ParentId.HasValue)
+                                        .OrderBy(x => x.ParentId)
+                                        .ToListAsync();
+
+            foreach (var subgroup in query)
+            {
+                list.Add(subgroup);
+                list.AddRange(await GetChildren(subgroup.Id));
+            }
+
+            return list;
+        }
+
+        private async Task<List<RecordOFXSubGroup>> GetChildren(int subGroupId, int level = 1)
+        {
+            var list = new List<RecordOFXSubGroup>();
+            var chd = await context.RecordOFXSubGroups
+                                    .Where(x => x.ParentId == subGroupId)
+                                    .ToListAsync();
+
+            foreach (var item in chd)
+            {
+                var levelDescription = "";
+
+                for (int i = 0; i < level; i++)
+                {
+                    levelDescription += "-";
+                }
+
+                list.Add(new RecordOFXSubGroup
+                {
+                    Id = item.Id,
+                    Description = $"{levelDescription} {item.Description}"
+                });
+
+                list.AddRange(await GetChildren(item.Id, level + 1));
+            }
+
+            return list;
         }
     }
 }
