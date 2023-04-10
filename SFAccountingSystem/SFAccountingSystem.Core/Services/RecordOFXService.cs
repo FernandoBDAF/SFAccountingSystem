5f56b90c6943ddac3b3c117fc991722b0ba051ab
lucasviniciusbatistacosta <lucasviniciusbatistacosta@gmail.com>
2023-04-10 19:22:46 -0300

    Improve record ofx.

--- SFAccountingSystem/SFAccountingSystem.Core/Services/RecordOFXService.cs
+++ SFAccountingSystem/SFAccountingSystem.Core/Services/RecordOFXService.cs
@@ -68,6 +68,7 @@ namespace SFAccountingSystem.Core.Services
                 return;
 
             record.Group = model.Group;
+            record.SubGroupId = null;
 
             await context.SaveChangesAsync();
         }
