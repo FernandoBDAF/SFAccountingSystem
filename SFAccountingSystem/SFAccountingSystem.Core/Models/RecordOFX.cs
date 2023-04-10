ca3af0143d6e7bae8755c58ea226c787e338b9c8
lucasviniciusbatistacosta <lucasviniciusbatistacosta@gmail.com>
2023-04-10 17:12:37 -0300

    Add new column.

--- SFAccountingSystem/SFAccountingSystem.Core/Models/RecordOFX.cs
+++ SFAccountingSystem/SFAccountingSystem.Core/Models/RecordOFX.cs
@@ -3,54 +3,56 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SFAccountingSystem.Core.Models
 {
-	public class RecordOFX : BaseModel
-	{
-		public RecordOFX() { }
+    public class RecordOFX : BaseModel
+    {
+        public RecordOFX() { }
 
-		public RecordOFX(TransactionOFX transaction, RecordOFXBank bank)
-		{
-			Value = transaction.Value;
-			Details = transaction.Description;
-			Date = transaction.Date.DateTime;
-			Bank = bank;
-			FITID = transaction.FITID;
+        public RecordOFX(TransactionOFX transaction, RecordOFXBank bank)
+        {
+            Value = transaction.Value;
+            Details = transaction.Description;
+            Date = transaction.Date.DateTime;
+            Bank = bank;
+            FITID = transaction.FITID;
 
-			if (transaction.Type == "CREDIT")
-			{
-				Type = RecordOFXType.Credit;
-			}
-			else
-			{
-				Type = RecordOFXType.Debit;
-			}
-		}
+            if (transaction.Type == "CREDIT")
+            {
+                Type = RecordOFXType.Credit;
+            }
+            else
+            {
+                Type = RecordOFXType.Debit;
+            }
+        }
 
-		public DateTime Date { get; set; }
+        public DateTime Date { get; set; }
 
-		public string? Details { get; set; }
+        public string? Details { get; set; }
 
-		public decimal Value { get; set; }
+        public decimal Value { get; set; }
 
-		public RecordOFXBank Bank { get; set; }
+        public RecordOFXBank Bank { get; set; }
 
-		public RecordOFXType Type { get; set; }
+        public RecordOFXType Type { get; set; }
 
-		[ForeignKey("Intermediation")]
-		public int? IntermediationId { get; set; }
+        [ForeignKey("Intermediation")]
+        public int? IntermediationId { get; set; }
 
-		public virtual Intermediation? Intermediation { get; set; }
+        public virtual Intermediation? Intermediation { get; set; }
 
-		[ForeignKey("User")]
-		public int? UserId { get; set; }
-		public virtual User? User { get; set; }
+        [ForeignKey("User")]
+        public int? UserId { get; set; }
+        public virtual User? User { get; set; }
 
-		public RecordOFXGroup? Group { get; set; }
+        public RecordOFXGroup? Group { get; set; }
 
-		[ForeignKey("RecordOFXSubGroup")]
-		public int? SubGroupId { get; set; }
+        [ForeignKey("RecordOFXSubGroup")]
+        public int? SubGroupId { get; set; }
 
-		public virtual RecordOFXSubGroup? RecordOFXSubGroup { get; set; }
+        public virtual RecordOFXSubGroup? RecordOFXSubGroup { get; set; }
 
-		public int FITID { get; set; }
-	}
+        public int FITID { get; set; }
+
+        public DateTime? ApprovedAt { get; set; }
+    }
 }
